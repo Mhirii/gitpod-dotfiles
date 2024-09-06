3f52fd02257ee3ee0dcca1bdd10db081d975739e
Mhirii <ahmedmhiri218@gmail.com>
2024-09-06 18:23:50 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-useNix=true
-
 current_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 dotfiles_source="${current_dir}/scripts"
 
 echo "Cloning neovim configuration..."
-git clone https://github.com/mhirii/lazyvim $HOME/.config/nvim
+git clone https://github.com/mhirii/lazyvim "$HOME"/.config/nvim
 nvim -c
 echo "Neovim configuration has been cloned."
 
 echo "Cloning tmux configuration..."
-git clone https://github.com/mhirii/tmux $HOME/.config/tmux
+git clone https://github.com/mhirii/tmux "$HOME"/.config/tmux
 nvim -c
 echo "Tmux configuration has been cloned."
 
@@ -19,12 +17,23 @@ echo "Installing tmux plugin manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Tmux plugin manager has been cloned."
 
-if [ "$useNix" = false ]; then
-  echo "Installing neovim..."
-  source "$dotfiles_source"/neovim.sh
-  echo "Neovim installation has finished."
-else
+if [ "$useNix" = true ]; then
   echo "Evaluating the nix dev-shell..."
   cd "$dotfiles_source" && nix develop || exit 0
   echo "The dev shell was installed successfully"
+else
+
+  echo "Installing neovim..."
+  cd $(mktemp -d) || exit 0
+  URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
+  if test -n "$NEOVIM_VERSION"; then
+    URL="https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim.appimage"
+  fi
+  curl -LO "$URL"
+  chmod u+x nvim.appimage
+  ./nvim.appimage --appimage-extract >/dev/null
+  mkdir -p /home/gitpod/.local/bin
+  ln -s "$(pwd)"/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
+  cd "$HOME" || exit 0
+  echo "Neovim installation has finished."
 fi
--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,13 +1,1 @@
 #!/usr/bin/env bash
-
-cd $(mktemp -d)
-URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
-if test -n "$NEOVIM_VERSION"; then
-  URL="https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim.appimage"
-fi
-
-curl -LO "$URL"
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract >/dev/null
-mkdir -p /home/gitpod/.local/bin
-ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
