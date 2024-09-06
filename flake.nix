7b7279b479633c29fc19742470c9afdd242ccdfb
Mhirii <ahmedmhiri218@gmail.com>
2024-09-06 19:32:30 +0100

    remove home manager

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
@@ -22,16 +18,11 @@
           fd
           lazygit
           lazydocker
+          fzf
         ];
         shellHook = ''
           tmux new-session -s fish
           '';
-      };
-      homeConfigurations.gitpod = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home.nix
-        ];
       };
     };
 }
--- install.sh
+++ install.sh
@@ -3,35 +3,28 @@
 # set in gitpod variables [OPTIONAL]
 # useNix=true or false
 # NEOVIM_VERSION
-# fish_shell=true or false
 
 current_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 dotfiles_source="${current_dir}/scripts"
 
 echo "Cloning neovim configuration..."
-git clone https://github.com/mhirii/lazyvim "$HOME"/.config/nvim
-echo "Neovim configuration has been cloned."
+git clone https://github.com/mhirii/lazyvim "$HOME"/.config/nvim &&
+  echo "Neovim configuration has been cloned."
 
 echo "Cloning tmux configuration..."
-git clone https://github.com/mhirii/tmux "$HOME"/.config/tmux
-echo "Tmux configuration has been cloned."
+git clone https://github.com/mhirii/tmux "$HOME"/.config/tmux &&
+  echo "Tmux configuration has been cloned."
 
 echo "Installing tmux plugin manager..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Tmux plugin manager has been cloned."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
+  echo "Tmux plugin manager has been cloned."
 
 # set useNix in gitpod variables
 if [ "$useNix" = true ]; then
   echo "Evaluating the nix dev-shell..."
-  nix develop $current_dir
-  echo "The dev shell was installed successfully"
-
-  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-  nix-channel --update
-  nix-shell '<home-manager>' -A install
-
+  nix develop "$current_dir" &&
+    echo "The dev shell was installed successfully"
 else
-
   echo "Installing neovim..."
   cd $(mktemp -d) || exit 0
   URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
@@ -47,5 +40,5 @@ else
   echo "Neovim installation has finished."
 fi
 
-echo " abbr v 'nvim'; abbr lg 'lazygit'; abbr lzd 'lazydocker'; abbr t 'tmux'; abbr tns 'tmux new-session -s'; abbr ta 'tmux attach'; abbr tls 'tmux list-sessions'; abbr tks 'tmux kill-session -t'; " >>"$HOME"/.config/fish/config.fish
-echo "fish_vi_key_bindings" >"$HOME"/.config/fish/config.fish
+echo " abbr v 'nvim'; abbr lg 'lazygit'; abbr lzd 'lazydocker'; abbr t 'tmux'; abbr tns 'tmux new-session -s'; abbr ta 'tmux attach'; abbr tls 'tmux list-sessions'; abbr tks 'tmux kill-session -t'; " >>"$HOME/.config/fish/config.fish"
+echo "fish_vi_key_bindings" >"$HOME/.config/fish/config.fish"
